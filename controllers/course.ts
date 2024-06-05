6bf0a04a98cfa01af42162a83a96766f6b1e2a6c
Genilso Carraro <71860731+Genilso190@users.noreply.github.com>
2024-06-05 01:42:40 +0000

    feat: Servidor

--- controllers/course.ts
+++ controllers/course.ts
@@ -5,12 +5,15 @@ export async function listCourse(req: Request, res: Response) {
   //conecta com o banco
   const client = await pool.connect();
   //realiza consulta sql
-  const courses = await client.query(`select * from courses`)
+  const courses = await client.query(`select * from course`)
   //retorna consulta em formato json
   return res.status(200).json(courses.rows);
 }
 export async function saveCourse(req: Request, res: Response) {
    const course = req.body;
+   //verifica o erro
+   console.log(course)
+   //conecta com o banco
   const client = await pool.connect();
   //realiza consulta sql
   const response = await client.query(`INSERT INTO courses (name) VALUES ('${course.name}')`)
---
+++ controllers/student.ts
@@ -0,0 +1,24 @@
+import { pool } from "../shared/database";
+import { Request, Response, Router } from "express";
+
+export async function listStudent(req: Request, res: Response) {
+  //conecta com o banco
+  const client = await pool.connect();
+  //realiza consulta sql
+  const students = await client.query(`select * from students`)
+  //retorna consulta em formato json
+  return res.status(200).json(students.rows);
+}
+export async function saveStudent(req: Request, res: Response) {
+   const student = req.body;
+   //verifica o erro
+   console.log(student)
+   //conecta com o banco
+  const client = await pool.connect();
+  //realiza consulta sql
+  const response = await client.query(`INSERT INTO students (name, email) VALUES ('${student.name}','${student.email}')`)
+ res.status(201).json(response);
+}
+
+
+
