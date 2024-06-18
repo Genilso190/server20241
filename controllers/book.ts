ee355778cae6e5496e5f26d406965e1b95f2e008
Genilso Carraro <71860731+Genilso190@users.noreply.github.com>
2024-06-18 00:25:20 +0000

    feat: book

--- controllers/book.ts
+++ controllers/book.ts
@@ -1,29 +1,29 @@
 import { pool } from "../shared/database";
 import { Request, Response, Router } from "express";
 
-export async function listBoock(req: Request, res: Response) {
+export async function listBook(req: Request, res: Response) {
   //conecta com o banco
   const client = await pool.connect();
   //realiza consulta sql
   try {
-    const boocks = await client.query(`select * from boocks`)
+    const books = await client.query(`select * from books`)
     //retorna consulta em formato json
-    return res.status(200).json(boocks.rows);
+    return res.status(200).json(books.rows);
   } catch (error) {
     res.status(400).json({ message: 'Dados inválidos:', error });
   } finally {
     client.release;
   }
 }
-export async function saveBoock(req: Request, res: Response) {
-  const boocks = req.body;
+export async function saveBook(req: Request, res: Response) {
+  const book = req.body;
   //verifica o erro
-  console.log(boocks)
+  console.log(book)
   //conecta com o banco
   const client = await pool.connect();
   try {
     //realiza consulta sql
-    const response = await client.query(`INSERT INTO boocks (titulo) VALUES ('${boocks.titulo}')`)
+    const response = await client.query(`INSERT INTO books (title, description) VALUES ('${book.description}')`)
     res.status(201).json(response);
   } catch (error) {
     res.status(400).json({ message: 'Dados inválidos:', error });
@@ -32,11 +32,11 @@ export async function saveBoock(req: Request, res: Response) {
   }
 }
 
-export async function deleteBoock(req: Request, res: Response){
+export async function deleteBook(req: Request, res: Response){
   const client = await pool.connect();
   const id = req.params.id
  try{ 
-  const response = await client.query(`delete from boocks where id=${id}`);
+  const response = await client.query(`delete from books where id=${id}`);
   res.status(200).json({message: "Registro Excluido"});
 }catch (error){ 
 res.status(400).json({message: "Erro na Exclusão", error});
